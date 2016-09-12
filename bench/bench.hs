f121610fc69a8fd90805384e1e42764bea7c7ae6
lyxia <li-yao.xia@ens.fr>
2016-09-12 16:35:50 +0100

    Benchmark containers Sequence

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,10 +1,14 @@
 {-# language RecordWildCards #-}
+{-# language BangPatterns #-}
 
 import Control.Monad (when)
 import Control.Monad.Random
 import Criterion.Measurement
 import Data.Foldable (forM_)
 import Text.Printf
+
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 
 import Data.Raz.Core
 
@@ -13,7 +17,7 @@ data BenchParams = BenchParams
   , rndSeed :: Int
   , tag :: String
   , testRaz :: Bool
-  , testFt :: Bool
+  , testSeq :: Bool
   , start :: Int
   , inserts :: Int
   , groups :: Int
@@ -27,7 +31,7 @@ defBenchParams = BenchParams
   , rndSeed = 0
   , tag = "None"
   , testRaz = True
-  , testFt = False
+  , testSeq = True
   , start = 0
   , inserts = 10000
   , groups = 10
@@ -43,11 +47,20 @@ time run = do
   return (stop - start, a)
 
 rndInsertRaz :: MonadRandom m => Int -> Int -> Raz Int -> m (Raz Int)
-rndInsertRaz _ 0 r = return r
-rndInsertRaz sz n r = do
+rndInsertRaz _ 0 !r = return r
+rndInsertRaz sz n !r = do
   p <- getRandomR (0, sz)
   r <- (insert L n . focus p . unfocus) r
   rndInsertRaz (sz+1) (n-1) r
+
+rndInsertSeq :: MonadRandom m => Int -> Int -> Seq Int -> m (Seq Int)
+rndInsertSeq _ 0 !sq = return sq
+rndInsertSeq sz n !sq = do
+  p <- getRandomR (0, sz)
+  let
+    (left, right) = Seq.splitAt p sq
+    sq' = (left Seq.|> n) Seq.>< right
+  rndInsertSeq (sz+1) (n-1) sq'
 
 printRow :: Int -> Int -> String -> String -> Int -> Int -> Int -> Double -> IO ()
 printRow = printf "%d,%d,%s,%s,%d,%d,%d,%.4f\n"
@@ -55,6 +68,7 @@ printRow = printf "%d,%d,%s,%s,%d,%d,%d,%.4f\n"
 main = do
   initializeTime
   r <- insert L 0 (singleton 0)
+  let sq = Seq.singleton 0 Seq.|> 0
   let BenchParams{..} = defBenchParams
   setStdGen (mkStdGen rndSeed)
   r <- if testRaz && start > 0
@@ -63,6 +77,12 @@ main = do
       printRow 0 rndSeed tag "RAZ" 0 0 start t
       return r
     else return r
+  sq <- if testSeq && start > 0
+    then do
+      (t, sq) <- time $ rndInsertSeq 0 start sq
+      printRow 0 rndSeed tag "SEQ" 0 0 start t
+      return sq
+    else return sq
   forM_ [1 .. reps] $ \i -> do
     let
       ins | multInserts = inserts * i
@@ -72,4 +92,10 @@ main = do
         (t, r) <- time $ rndInsertRaz size ins r
         printRow 0 rndSeed tag "RAZ" i size ins t
         seqRaz (size + ins) (repeats - 1) r
+      seqSeq size 0 sq = return ()
+      seqSeq size repeats sq = do
+        (t, sq) <- time $ rndInsertSeq size ins sq
+        printRow 0 rndSeed tag "SEQ" i size ins t
+        seqSeq (size + ins) (repeats - 1) sq
     when testRaz $ seqRaz start groups r
+    when testSeq $ seqSeq start groups sq
